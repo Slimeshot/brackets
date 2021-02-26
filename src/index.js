acfcffea3f8b92172aa638ff1c10552c6b313475
Evgeny Hohlov <slimeshot@mail.ru>
2021-02-26 15:57:15 +0300

    feat: add completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strConfig = bracketsConfig.map(item => item.join(''));
+  
+  console.log(str);
+  
+  for (let i = 0; i < strConfig.length; i++) {
+    if (str.includes(strConfig[i])) {
+      str = str.replace(strConfig[i], '');
+      i = -1;
+    }
+
+  }
+  
+  return str.length === 0 ? true : false;
 }
